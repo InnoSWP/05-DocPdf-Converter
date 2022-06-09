0233638d1dfb2540160d1eb19dc978c18768b5ce
zaqbez39me <korolov_k@mail.ru>
2022-06-10 02:59:48 +0300

    Added windows algorithm

--- main/algorithm.py
+++ main/algorithm.py
@@ -1,3 +1,5 @@
+from os import makedirs, path
+from pathlib import Path
 from zipfile import ZipFile
 from sys import platform
 import subprocess
@@ -13,18 +15,47 @@ def zip_files_in_dir(filepath: str, files: [str], zip_file_name: str):
 
 def convert(filepath: str, files: [str], index):
     if platform == "linux" or platform == "linux2":
-        convert_linux(filepath, files, index)
-
+        return convert_linux(filepath, files, index)
     elif platform == "win32":
-        pass
+        convert_windows(filepath, files, index)
+
+
+def windows(paths):
+    import win32com.client
+    word = win32com.client.Dispatch("Word.Application")
+    wd_format_pdf = 17
+
+    if paths["batch"]:
+        for docx_filepath in sorted(Path(paths["input"]).glob("*.docx")):
+            pdf_filepath = Path(paths["output"]) / (str(docx_filepath.stem) + ".pdf")
+            doc = word.Documents.Open(str(docx_filepath))
+            doc.SaveAs(str(pdf_filepath), FileFormat=wd_format_pdf)
+            doc.Close(0)
+    else:
+        docx_filepath = Path(paths["input"]).resolve()
+        pdf_filepath = Path(paths["output"]).resolve()
+        doc = word.Documents.Open(str(docx_filepath))
+        doc.SaveAs(str(pdf_filepath), FileFormat=wd_format_pdf)
+        doc.Close(0)
+
+
+def convert_windows(filepath: str, files: [str], index: int):
+    from docx2pdf import resolve_paths
+    if not len(files):
+        return []
+    converted_file_path = f'{path.dirname(__file__)}/converted_files/{index}/'
+    makedirs(converted_file_path, exist_ok=True)
+    paths = resolve_paths(filepath, converted_file_path)
+    windows(paths)
 
 
 def convert_linux(filepath: str, files: [str], index: int):
     if not len(files):
         return []
-    else:
-        cmd = f'cd {filepath}converted_files/{index}/'
-        for file in files:
-            cmd += f' && lowriter --convert-to pdf {file} && rm file_name'
-        subprocess.call(cmd, shell=True)
-
+    converted_file_path = f'{path.dirname(__file__)}/converted_files/{index}/'
+    cmd = f'cd {filepath}'
+    makedirs(converted_file_path, exist_ok=True)
+    for file in files:
+        cmd += f' && lowriter --convert-to pdf {file} --outdir {converted_file_path}'
+    subprocess.call(cmd, shell=True)
+    return converted_file_path
