41a71418308b933395ed4e6ced16f88fb741ea16
zaqbez39me <korolov_k@mail.ru>
2022-06-09 15:03:11 +0300

    Made files saving properly that makes it easy to remove them after conversion

--- main/api.py
+++ main/api.py
@@ -1,9 +1,10 @@
 import shutil
+
 from rest_framework import generics, permissions, mixins, settings, status
 from rest_framework.response import Response
-
+from os import makedirs, path
 from .algorithm import convert
-from .models import Converter
+from .models import Converter, Conversion
 from .serializers import ConvertSerializer
 
 
@@ -16,11 +17,19 @@ class ConvertApi(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         if 'token' in request.data:
             files = request.FILES.getlist('files')
+            conversion = Conversion()
+            conversion.save()
+            last_id = Conversion.objects.latest('id').id
+            filepath = f'{path.dirname(__file__)}/files/{last_id}/'
             for file in files:
-                with open(file.name, 'wb') as out_file:
+                makedirs(filepath, exist_ok=True)
+                filename = f'{filepath}{file.name}'
+                with open(filename, 'wb') as out_file:
                     shutil.copyfileobj(file, out_file)
+            file_names = [file.name for file in files]
+            convert([f'{filepath}{file_name}' for file_name in file_names])
             return Response({
-                "files": f'{convert(files)}'
+                "files": f'{[file_name for file_name in file_names]}'
             })
         else:
             return Response({
--- main/models.py
+++ main/models.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Conversion(models.Model):
+    files_number = models.IntegerField
+
 class Upload(models.Model):
     title = models.CharField(max_length=30)
 
