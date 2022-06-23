e66b6034f02e2ef25d50a160e4bc2a8de95d4c90
zaqbez39me <korolov_k@mail.ru>
2022-06-23 21:31:04 +0300

    Reformatted code with local Pylint

--- main/api.py
+++ main/api.py
@@ -47,6 +47,7 @@ class ConvertApi(generics.GenericAPIView):
             serializer = self.get_serializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             files = request.FILES.getlist("files")
+            print(files)
             Conversion().save()
             # Get this conversion operation id.
             last_id = Conversion.objects.latest("id").id
