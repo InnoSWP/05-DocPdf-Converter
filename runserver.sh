87637cbf92f82f43b942499725df8448773d7161
Ario <kurdyukovalexander123@gmail.com>
2022-06-29 22:32:44 +0300

    Revert "Added sh script"
    This reverts commit 35be1c01c3984cab5ab563babff3dc113b486768.

--- runserver.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt install python3 && sudo apt install python3-venv && python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt && python3 manage.py migrate && python3 manage.py makemigrations && python3 manage.py makemigrations main && python3 manage.py migrate && python3 manage.py runserver
