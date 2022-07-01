b23b4f1fba5599ae2a86cef3714b9a5feeaf7069
zaqbez39me <korolov_k@mail.ru>
2022-07-01 13:32:58 +0300

    added tests for new logic

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,6 +8,22 @@ from env_consts import OS_SLASH
 from main.api import get_init_id
 
 
+def send_single_file_response(root, client, convert_link, file_name):
+    """
+
+    :param root: root directory with file
+    :param client: client sender
+    :param convert_link: link for sending file
+    :param file_name: name of sending file
+    :return: response of request
+    """
+    with open(f"{root}{OS_SLASH}{file_name}", "rb") as files:
+        resp = client.post(
+            convert_link, {"name": "fred", "files": files, "attachment": files}
+        )
+    return resp
+
+
 class ApiTestCase(TestCase):
     """
     class with all test algorithms
@@ -25,28 +41,46 @@ class ApiTestCase(TestCase):
         :return:
         """
         files = []
-        for file_name in ["1.docx", "2.docx", "3.docx", "4.docx"]:
+        for file_name in ["1.docx", "2.docx", "3.docx", "4.docx", "1.xlsx"]:
             files.append(open(f"{self.root}{OS_SLASH}{file_name}", "rb"))
         resp = self.client.post(
             self.convert_link, {"name": "fred", "files": files, "attachment": files}
         )
         for file in files:
             file.close()
-        self.assertEqual(resp.headers["files"], f"attachment; filename=result_{get_init_id()}.zip")
+        self.assertEqual(
+            resp.headers["files"], f"attachment; filename=result_{get_init_id()}.zip"
+        )
 
-    def test_convert_post_one(self):
+    def test_convert_post_docx(self):
         """
-        test method for post requests
+        test method for post request with single docx
 
         :return:
         """
         file_name = "1.docx"
-        with open(f"{self.root}{OS_SLASH}{file_name}", "rb") as files:
-            resp = self.client.post(
-                self.convert_link, {"name": "fred", "files": files, "attachment": files}
-            )
+        resp = send_single_file_response(
+            self.root, self.client, self.convert_link, file_name
+        )
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.headers["files"], f"attachment; filename=result_{get_init_id()}.pdf")
+        self.assertEqual(
+            resp.headers["files"], f"attachment; filename=result_{get_init_id()}.pdf"
+        )
+
+    def test_convert_post_xlsx(self):
+        """
+        test method for post request with single xlsx
+
+        :return:
+        """
+        file_name = "1.xlsx"
+        resp = send_single_file_response(
+            self.root, self.client, self.convert_link, file_name
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp.headers["files"], f"attachment; filename=result_{get_init_id()}.pdf"
+        )
 
     def test_convert_get_local(self):
         """
@@ -66,9 +100,11 @@ class ApiTestCase(TestCase):
         :return:
         """
         response = self.client.get(
-            self.convert_link, follow=True, HTTP_X_FORWARDED_FOR=".".join(map(str,
-                                          (random.randint(0, 255) for _ in
-                                           range(4))))
+            self.convert_link,
+            follow=True,
+            HTTP_X_FORWARDED_FOR=".".join(
+                map(str, (random.randint(0, 255) for _ in range(4)))
+            ),
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers["Content-Type"], "application/json")
