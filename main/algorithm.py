f4727f837e5346499d358ac08132fe209c4d1367
zaqbez39me <korolov_k@mail.ru>
2022-07-01 14:29:44 +0300

    Split windows and linux logic on files

--- main/algorithm.py
+++ main/algorithm.py
@@ -1,9 +1,6 @@
-import subprocess
-import time
 from mimetypes import MimeTypes
 from os import makedirs, path, rename
 from os.path import basename
-from pathlib import Path
 from shutil import copyfileobj
 from sys import platform
 from zipfile import ZipFile
@@ -11,6 +8,8 @@ from zipfile import ZipFile
 from django.http import HttpResponse
 
 import env_consts as ec
+from main.linux import convert_linux
+from main.windows import convert_windows
 
 
 def save_files(files, last_id: int):
@@ -90,6 +89,23 @@ def get_file_response(file_path: str, file_name: str):
     response["files"] = f"attachment; filename={file_name}"
     return response
 
+def get_converted_file_path(index: int):
+    """
+    Function to get converted path via index.
+    :param index: index of converted path directory
+    :type index: int
+    :return: converted path directory
+    :rtype: str
+    """
+    # Path to converted files.
+    converted_file_path = (
+        f"{path.dirname(__file__)}{ec.OS_SLASH}"
+        f"converted_files{ec.OS_SLASH}{index}{ec.OS_SLASH}"
+    )
+    # Make this directory if it doesn't exist.
+    makedirs(converted_file_path, exist_ok=True)
+    return converted_file_path
+
 
 def convert(filepath: str, files, index, has_type_in_request):
     """
@@ -112,175 +128,3 @@ def convert(filepath: str, files, index, has_type_in_request):
     elif platform == "win32":
         convert_windows(filepath, files, converted_file_path, has_type_in_request)
     return converted_file_path
-
-
-def convert_windows(
-    filepath: str, files, converted_file_path: str, has_type_in_request: dict
-):
-    """
-    Windows conversion core.
-
-    :param has_type_in_request: flag of each type in request
-    :param converted_file_path: path to output files
-    :type converted_file_path: str
-    :param filepath: path to input files
-    :type filepath: str
-    :param files: file names
-    :type files: list of str
-    :return: path to converted files
-    :rtype: str
-    """
-    from docx2pdf import resolve_paths
-
-    if not files:
-        return
-    paths = resolve_paths(filepath, converted_file_path)
-    windows(paths, files, has_type_in_request)
-
-
-def windows_convert_docx(word, docx_filepath: str, pdf_filepath: str, pdf_format: int):
-    """
-    Windows conversion docx part.
-
-    :param word: Word application
-    :param docx_filepath: path to docx file
-    :type docx_filepath: str
-    :param pdf_filepath: path to future pdf file
-    :type pdf_filepath: str
-    :param pdf_format: pdf format code
-    :type pdf_format: int
-    """
-
-    doc = word.Documents.Open(docx_filepath)
-    doc.SaveAs(pdf_filepath, FileFormat=pdf_format)
-    doc.Close(0)
-
-
-def windows_convert_xlsx(excel, xlsx_filepath: str, pdf_filepath: str):
-    """
-    Windows conversion xlsx part.
-
-    :param excel: Excel application
-    :param xlsx_filepath: path to xlsx file
-    :type xlsx_filepath: str
-    :param pdf_filepath: path to future pdf file
-    :type pdf_filepath: str
-    """
-    xl_sheets = excel.Workbooks.Open(xlsx_filepath)
-    xl_sheets.Worksheets[0].ExportAsFixedFormat(0, pdf_filepath)
-    xl_sheets.Close(True)
-
-
-def windows(paths, files, has_type_in_request: dict):
-    """
-    Conversion algorithm for Windows OS.
-
-    :param has_type_in_request: flag of each type in request
-    :param paths: paths
-    :type paths: dict[str, Union[bool, str, Path]]
-    :param files: files for conversion
-    :type files: list of str
-    :return:
-    """
-    import win32com.client as w32c
-    from servicemanager import CoInitializeEx
-
-    CoInitializeEx(0)
-    # Open word application for conversion.
-    word = None
-    excel = None
-    docx_type = ".docx"
-    xlsx_type = ".xlsx"
-    if has_type_in_request[docx_type]:
-        word = w32c.Dispatch("Word.Application")
-    # Open excel application for conversion.
-    if has_type_in_request[xlsx_type]:
-        excel = w32c.Dispatch("Excel.Application")
-    # Format of PDF file.
-    wd_format_pdf = 17
-    # Convert each file via word application.
-    for file in files:
-        document_filepath = f"{paths['input']}{ec.OS_SLASH}{file}"
-        pdf_filepath = (
-            f"{paths['output']}{ec.OS_SLASH}{Path(document_filepath).stem}.pdf"
-        )
-        if docx_type in file:
-            windows_convert_docx(word, document_filepath, pdf_filepath, wd_format_pdf)
-        elif xlsx_type in file:
-            windows_convert_xlsx(excel, document_filepath, pdf_filepath)
-    # Close word application.
-    if has_type_in_request[docx_type]:
-        word.Quit()
-    # Close excel application.
-    if has_type_in_request[xlsx_type]:
-        excel.Quit()
-
-
-def get_converted_file_path(index: int):
-    """
-    Function to get converted path via index.
-    :param index: index of converted path directory
-    :type index: int
-    :return: converted path directory
-    :rtype: str
-    """
-    # Path to converted files.
-    converted_file_path = (
-        f"{path.dirname(__file__)}{ec.OS_SLASH}"
-        f"converted_files{ec.OS_SLASH}{index}{ec.OS_SLASH}"
-    )
-    # Make this directory if it doesn't exist.
-    makedirs(converted_file_path, exist_ok=True)
-    return converted_file_path
-
-
-def install_libre():
-    """
-    install libre on environment
-    :return:
-    """
-    subprocess.run(
-        f"echo {ec.SYS_PWD}|sudo -S apt -y update && sudo -S apt -y install libreoffice",
-        shell=True,
-        check=True,
-    )
-    ec.INSTALLED_LIBRE = True
-
-
-def convert_linux(filepath: str, files, converted_file_path: str, has_type_in_request):
-    """
-    Conversion algorithm for Linux-like OS.
-
-    :param has_type_in_request:
-    :param converted_file_path: path to output files
-    :type converted_file_path: str
-    :param filepath: path to input files
-    :type filepath: str
-    :param files: file names
-    :type files: list of str
-    :return: path to converted files
-    :rtype: str
-    """
-    start = time.time()
-    if not files:
-        return
-    if not ec.INSTALLED_LIBRE:
-        with subprocess.Popen(
-            "apt list -a libreoffice",
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        ) as process:
-            if "installed" not in process.communicate()[0].decode("utf-8"):
-                install_libre()
-
-    command = f"cd {filepath}"
-    if has_type_in_request[".docx"]:
-        command += f"&& lowriter --headless --convert-to pdf *.docx --outdir {converted_file_path}"
-    if has_type_in_request[".xlsx"]:
-        command += f"&& lowriter --headless --convert-to pdf *.xlsx --outdir {converted_file_path}"
-    makedirs(converted_file_path, exist_ok=True)
-    # For all files call lowriter for conversion (LibreOffice).
-    # Execute all console commands.
-    subprocess.run(command, shell=True, check=True)
-    print(time.time() - start)
---
+++ main/windows.py
@@ -0,0 +1,105 @@
+from pathlib import Path
+
+import win32com.client as w32c
+from servicemanager import CoInitializeEx
+
+import env_consts as ec
+
+
+def convert_windows(
+    filepath: str, files, converted_file_path: str, has_type_in_request: dict
+):
+    """
+    Windows conversion core.
+
+    :param has_type_in_request: flag of each type in request
+    :param converted_file_path: path to output files
+    :type converted_file_path: str
+    :param filepath: path to input files
+    :type filepath: str
+    :param files: file names
+    :type files: list of str
+    :return: path to converted files
+    :rtype: str
+    """
+    from docx2pdf import resolve_paths
+
+    if not files:
+        return
+    paths = resolve_paths(filepath, converted_file_path)
+    windows(paths, files, has_type_in_request)
+
+
+def windows_convert_docx(word, docx_filepath: str, pdf_filepath: str, pdf_format: int):
+    """
+    Windows conversion docx part.
+
+    :param word: Word application
+    :param docx_filepath: path to docx file
+    :type docx_filepath: str
+    :param pdf_filepath: path to future pdf file
+    :type pdf_filepath: str
+    :param pdf_format: pdf format code
+    :type pdf_format: int
+    """
+
+    doc = word.Documents.Open(docx_filepath)
+    doc.SaveAs(pdf_filepath, FileFormat=pdf_format)
+    doc.Close(0)
+
+
+def windows_convert_xlsx(excel, xlsx_filepath: str, pdf_filepath: str):
+    """
+    Windows conversion xlsx part.
+
+    :param excel: Excel application
+    :param xlsx_filepath: path to xlsx file
+    :type xlsx_filepath: str
+    :param pdf_filepath: path to future pdf file
+    :type pdf_filepath: str
+    """
+    xl_sheets = excel.Workbooks.Open(xlsx_filepath)
+    xl_sheets.Worksheets[0].ExportAsFixedFormat(0, pdf_filepath)
+    xl_sheets.Close(True)
+
+
+def windows(paths, files, has_type_in_request: dict):
+    """
+    Conversion algorithm for Windows OS.
+
+    :param has_type_in_request: flag of each type in request
+    :param paths: paths
+    :type paths: dict[str, Union[bool, str, Path]]
+    :param files: files for conversion
+    :type files: list of str
+    :return:
+    """
+
+    CoInitializeEx(0)
+    # Open word application for conversion.
+    word = None
+    excel = None
+    docx_type, xlsx_type = ".docx", ".xlsx"
+    if has_type_in_request[docx_type]:
+        word = w32c.Dispatch("Word.Application")
+    # Open excel application for conversion.
+    if has_type_in_request[xlsx_type]:
+        excel = w32c.Dispatch("Excel.Application")
+    # Format of PDF file.
+    wd_format_pdf = 17
+    # Convert each file via word application.
+    for file in files:
+        document_filepath = f"{paths['input']}{ec.OS_SLASH}{file}"
+        pdf_filepath = (
+            f"{paths['output']}{ec.OS_SLASH}{Path(document_filepath).stem}.pdf"
+        )
+        if docx_type in file:
+            windows_convert_docx(word, document_filepath, pdf_filepath, wd_format_pdf)
+        elif xlsx_type in file:
+            windows_convert_xlsx(excel, document_filepath, pdf_filepath)
+    # Close word application.
+    if has_type_in_request[docx_type]:
+        word.Quit()
+    # Close excel application.
+    if has_type_in_request[xlsx_type]:
+        excel.Quit()
