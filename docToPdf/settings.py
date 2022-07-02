2e849e47797d6a16a198bd0f8a30b469d6a46b61
zaqbez39me <korolov_k@mail.ru>
2022-07-02 22:16:57 +0300

    Deploy on heroku

--- docToPdf/settings.py
+++ docToPdf/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import dj_database_url
+
+db_from_env = dj_database_url.config()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,10 +28,7 @@ SECRET_KEY = 'django-insecure-!1gbh31l-g6b6q9(fzm#xbt3fehls7ks1gzs&vd)=ne!uz=#lf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['*']
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
 # Application definition
@@ -78,10 +79,7 @@ WSGI_APPLICATION = 'docToPdf.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': db_from_env
 }
 
 REST_FRAMEWORK = {
@@ -125,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
