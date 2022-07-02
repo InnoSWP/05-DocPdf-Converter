d883d96a8b78fb929b144a0196011b3a1baf05af
osx11 <osx11@ya.ru>
2022-07-02 22:51:04 +0300

    fixed imports for linter

--- docToPdf/settings.py
+++ docToPdf/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
