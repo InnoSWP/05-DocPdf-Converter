eb126669021c683e5edc13a3e031ea69f6d5b633
osx11 <osx11@ya.ru>
2022-07-02 22:44:32 +0300

    trying to change base_dir to deploy to heroku

--- docToPdf/settings.py
+++ docToPdf/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'docToPdf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
